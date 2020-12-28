fcd10332228cbfb5ab21794d9e31104e255fa313
nadavspinco <nadavspin@gmail.com>
2020-12-28 15:42:31 +0200

    update engine proxy

--- Server/Server.java
+++ Server/Server.java
@@ -1,5 +1,6 @@
 package Server;
 
+import Logic.SystemManagement;
 import com.sun.corba.se.impl.orbutil.ObjectWriter;
 
 import java.io.*;
@@ -9,11 +10,11 @@ import java.net.Socket;
 public class Server {
     private int port = 1989;
     private boolean serverAlive = true;
+    private SystemManagement systemManagement = new SystemManagement();
 
     public static void main(String[] args) throws IOException {
        Server server = new Server();
        server.runServer(args);
-
     }
 
     private void runServer(String[] args) throws IOException {
@@ -24,21 +25,43 @@ public class Server {
     private void runServerSocket(ServerSocket serverSocket) throws IOException {
         while (serverAlive) {
             Socket socket = serverSocket.accept();
-
-            try (BufferedReader in =
-                         new BufferedReader(
-                                 new InputStreamReader(
-                                         socket.getInputStream()));
-                 PrintWriter out =
-                         new PrintWriter(
-                                 socket.getOutputStream(),
-                                 true)) {
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+            new Thread(()-> {
+                try(ObjectInputStream in = new ObjectInputStream(socket.getInputStream());  //exception 1
+                    ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
+                    Object obj;
+                    ServerRequest request;
+                    try{    //exception 2
+                        while((obj = in.readObject()) != null) {
+                            if ((request = convertObjectToRequest(obj)) != null)
+                                executeRequest(request);
+                        }
+                    }
+                    catch (ClassNotFoundException e) {  //exception 2
+                        e.printStackTrace();
+                    }
+                    catch (IOException e) {  //exception 2
+                        e.printStackTrace();
+                    }
+                }
+                catch (IOException e) { //exception 1
+                    e.printStackTrace();
+                }
+            }).run();
         }
+
     }
+    private void executeRequest(ServerRequest request){
+
+    }
+
+    private ServerRequest convertObjectToRequest(Object obj) {
+        ServerRequest serverRequest = null;
+        if(obj instanceof ServerRequest){
+            serverRequest = (ServerRequest)obj;
+        }
+        return serverRequest;
+    }
+
     private void setPortInput(String[] args) {
         if(args.length > 0){
             setPort(args[0]);
